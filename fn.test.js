1ba8d9a5d8f43d255f86cca476b517ae83bb90e0
5Suminsss2 <sumin.kim.dev@gmail.com>
2023-04-25 14:55:39 +0900

    4강 : 테스트 전후 작업 beforeEach와 afterEach

--- fn.test.js
+++ fn.test.js
@@ -1,12 +1,33 @@
 const fn = require("./fn");
 
-// 비동기 코드 테스트
-// async 형태의 비동기 테스트일 경우, jest는 promise가 리턴되면 resolve될 때까지 기다려줌
-// done 해줄 필요x (callback 형태일 경우만 done넘겨주기)
-// return 대신 await를 씀
+// 테스트 전후 작업
+// 초기화가 안되는 변수를 초기화 해야할때 사용하는 beforeEach, afterEach
+// beforeEach : 테스트 전
+// afterEach : 테스트 후
+// beforeEach를 안쓰면 num이 계속 저장되어서 0 1 3 6 10 이렇게 돼버린다
 
-test("3초 후에 받아온 나이는 30", async () => {
-  //   const age = await fn.getAge();
-  //   expect(age).toBe(30);
-  await expect(fn.getAge()).resolves.toBe(30);
+let num = 10;
+
+beforeEach(() => {
+  num = 0;
+});
+
+test("0 더하기 1은 1이야", async () => {
+  num = fn.add(num, 1);
+  expect(num).toBe(1);
+});
+
+test("0 더하기 2은 2이야", async () => {
+  num = fn.add(num, 1);
+  expect(num).toBe(1);
+});
+
+test("0 더하기 3은 3이야", async () => {
+  num = fn.add(num, 1);
+  expect(num).toBe(1);
+});
+
+test("0 더하기 4은 4이야", async () => {
+  num = fn.add(num, 1);
+  expect(num).toBe(1);
 });
