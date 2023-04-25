7463b66c156074602c1757b3a31dd6b21e994d16
5Suminsss2 <sumin.kim.dev@gmail.com>
2023-04-25 10:57:33 +0900

    2강 : toBe와 toEqual의 차이점

--- fn.test.js
+++ fn.test.js
@@ -1,26 +1,19 @@
 const fn = require("./fn");
 
-// toBe는 matcher이다.
-// matcher은 숫자열이나 문자열 등 기본 타입값을 비교할 때 사용
-
-// 성공
-test("1은 1이야", () => {
-  // expect에 검증할 값을 넣어주고, toBe에 기대되는 값을 넣어줌
-  expect(1).toBe(1);
+// toBe와 toEqual의 차이점
+// toBe 는 객체나 배열은 재귀적으로 돌면서 값을 확인해야하기 때문에 toEqual을 써줘야함
+test("이름과 나이를 전달받아서 객체를 반환해줘", () => {
+  expect(fn.makeUser("Mike", 30)).toBe({
+    // 실패
+    name: "Mike",
+    age: 30,
+  });
 });
 
-test("2 더하기 3은 5야", () => {
-  // expect에 검증할 값을 넣어주고, toBe에 기대되는 값을 넣어줌
-  expect(fn.add(2, 3)).toBe(5);
-});
-
-test("3 더하기 3은 5가 아니야", () => {
-  // expect에 검증할 값을 넣어주고, toBe에 기대되는 값을 넣어줌
-  expect(fn.add(3, 3)).not.toBe(5);
-});
-
-// 실패
-test("3 더하기 3은 5야", () => {
-  // expect에 검증할 값을 넣어주고, toBe에 기대되는 값을 넣어줌
-  expect(fn.add(3, 3)).toBe(5);
+test("이름과 나이를 전달받아서 객체를 반환해줘", () => {
+  expect(fn.makeUser("Mike", 30)).toEqual({
+    // 성공
+    name: "Mike",
+    age: 30,
+  });
 });
