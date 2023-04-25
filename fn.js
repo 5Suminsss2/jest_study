569815ab4c6d0c4f95c4c4b9da19b8873b93e47b
5Suminsss2 <sumin.kim.dev@gmail.com>
2023-04-25 14:17:44 +0900

    3강 : promise 형태의 비동기 코드 테스트하기 (resolve 테스트)

--- fn.js
+++ fn.js
@@ -11,9 +11,17 @@ const fn = {
   getName: (callback) => {
     const name = "Mike";
     setTimeout(() => {
-      // callback(name);
-      throw new Error("서버 에러...");
+      callback(name);
+      // throw new Error("서버 에러...");
     }, 3000);
+  },
+  getAge: () => {
+    const age = 30;
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        res(age);
+      }, 3000);
+    });
   },
 };
 
