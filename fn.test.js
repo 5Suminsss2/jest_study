eb6c96afd6dd43b9b8a23720ac5f6238b45c789f
5Suminsss2 <sumin.kim.dev@gmail.com>
2023-04-25 16:18:20 +0900

    5강 : mock 함수의 return - mockReturnValueOnce

--- fn.test.js
+++ fn.test.js
@@ -3,13 +3,22 @@ const fn = require("./fn");
 // Mock Function 목 함수
 // callback함수를 사용하지 않고 목함수를 이용해도 잘 동작하고 코드도 간결해짐
 // mock.results에는 return 된 값이 남아있음
+// mockReturnValue : 실행할때마다 각각 다른 리턴값을 내줌
+// mockReturnValueOnce : 중간에 return 값을 바꿀때 사용
 
 // 목함수 생성
 const mockFn = jest.fn((num) => num + 1);
 
-mockFn(10);
-mockFn(20);
-mockFn(30);
+mockFn
+  .mockReturnValueOnce(10)
+  .mockReturnValueOnce(20)
+  .mockReturnValueOnce(30)
+  .mockReturnValue(40);
+
+mockFn();
+mockFn();
+mockFn();
+mockFn();
 
 test("10에서 1 증가한 값이 반환됨 ", () => {
   expect(mockFn.mock.results[0].value).toBe(11);
