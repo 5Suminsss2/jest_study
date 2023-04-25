14e20646a82537461bf3253649114025fb079c71
5Suminsss2 <sumin.kim.dev@gmail.com>
2023-04-25 16:22:20 +0900

    5강 : mockReturnValueOnce 활용

--- fn.test.js
+++ fn.test.js
@@ -1,32 +1,19 @@
 const fn = require("./fn");
 
 // Mock Function 목 함수
-// callback함수를 사용하지 않고 목함수를 이용해도 잘 동작하고 코드도 간결해짐
-// mock.results에는 return 된 값이 남아있음
-// mockReturnValue : 실행할때마다 각각 다른 리턴값을 내줌
-// mockReturnValueOnce : 중간에 return 값을 바꿀때 사용
+// mockReturnValueOnce 활용해보자 -> 홀짝 구분하기 (따로 callback 함수 만들지 않고 mockFn으로 사용해보자)
 
-// 목함수 생성
-const mockFn = jest.fn((num) => num + 1);
+const mockFn = jest.fn();
 
 mockFn
-  .mockReturnValueOnce(10)
-  .mockReturnValueOnce(20)
-  .mockReturnValueOnce(30)
-  .mockReturnValue(40);
+  .mockReturnValueOnce(true)
+  .mockReturnValueOnce(false)
+  .mockReturnValueOnce(true)
+  .mockReturnValueOnce(false)
+  .mockReturnValue(true);
 
-mockFn();
-mockFn();
-mockFn();
-mockFn();
+const result = [1, 2, 3, 4, 5].filter((num) => mockFn(num));
 
-test("10에서 1 증가한 값이 반환됨 ", () => {
-  expect(mockFn.mock.results[0].value).toBe(11);
-});
-
-test("20에서 1 증가한 값이 반환됨 ", () => {
-  expect(mockFn.mock.results[1].value).toBe(21);
-});
-test("30에서 1 증가한 값이 반환됨 ", () => {
-  expect(mockFn.mock.results[2].value).toBe(31);
+test("홀수는 1,3,5", () => {
+  expect(result).toStrictEqual([1, 3, 5]);
 });
