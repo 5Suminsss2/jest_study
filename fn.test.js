517b5b76bfa21557d763f07cdd8f93c1eab05531
5Suminsss2 <sumin.kim.dev@gmail.com>
2023-04-25 16:24:57 +0900

    5강 : mockResolvedValue

--- fn.test.js
+++ fn.test.js
@@ -1,19 +1,14 @@
 const fn = require("./fn");
 
 // Mock Function 목 함수
-// mockReturnValueOnce 활용해보자 -> 홀짝 구분하기 (따로 callback 함수 만들지 않고 mockFn으로 사용해보자)
+// mockResolvedValue : 비동기 함수를 흉내낼 수 있음
 
 const mockFn = jest.fn();
 
-mockFn
-  .mockReturnValueOnce(true)
-  .mockReturnValueOnce(false)
-  .mockReturnValueOnce(true)
-  .mockReturnValueOnce(false)
-  .mockReturnValue(true);
-
-const result = [1, 2, 3, 4, 5].filter((num) => mockFn(num));
+mockFn.mockResolvedValue({ name: "Mike" });
 
 test("홀수는 1,3,5", () => {
-  expect(result).toStrictEqual([1, 3, 5]);
+  mockFn().then((res) => {
+    expect(res.name).toBe("Mike");
+  });
 });
