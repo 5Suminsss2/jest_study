2027ecb0e03427e46736ecb1784291adc364109b
5Suminsss2 <sumin.kim.dev@gmail.com>
2023-04-25 14:10:34 +0900

    3강 : 서버 에러가 난 callback 패턴 비동기 코드를 테스트 해보기

--- fn.test.js
+++ fn.test.js
@@ -5,8 +5,12 @@ const fn = require("./fn");
 
 test("3초 후에 받아온 이름은 Mike", (done) => {
   function callback(name) {
-    expect(name).toBe("Mike");
-    done();
+    try {
+      expect(name).toBe("Mike");
+      done();
+    } catch (error) {
+      done();
+    }
   }
   fn.getName(callback);
 });
