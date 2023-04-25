33af0cb38bdca037bf8d373e6604250db436faa5
5Suminsss2 <sumin.kim.dev@gmail.com>
2023-04-25 14:16:32 +0900

    3강 : promise 형태의 비동기 코드 테스트하기 (resolve 테스트)

--- fn.test.js
+++ fn.test.js
@@ -1,16 +1,13 @@
 const fn = require("./fn");
 
 // 비동기 코드 테스트
-// done을 인자로 받아와서 호출하면 비동기 코드 테스트 가능 (안쓰면 1초만에 pass 하고 난리남)
+// promise 형태의 비동기 테스트일 경우, jest는 promise가 리턴되면 resolve될 때까지 기다려줌
+// done 해줄 필요x (callback 형태일 경우만 done넘겨주기)
+// promise를 사용하면 return 꼭 해줘야함
 
-test("3초 후에 받아온 이름은 Mike", (done) => {
-  function callback(name) {
-    try {
-      expect(name).toBe("Mike");
-      done();
-    } catch (error) {
-      done();
-    }
-  }
-  fn.getName(callback);
+test("3초 후에 받아온 나이는 30", (done) => {
+  return fn.getAge().then((age) => {
+    expect(age).toBe(30);
+  });
+  // return expect(fn.getAge()).resolves.toBe(30); // 더 간결하게 resolve 테스트하는 방법
 });
